80159ff0bc1e9098802608eec9928c0163d949f5
brinxmat <789709+brinxmat@users.noreply.github.com>
2025-05-27 08:00:01 +0200

    Refactor database code for ease of reading

--- src/test/java/helltech/dynamodb/DynamoDBLocal.java
+++ src/test/java/helltech/dynamodb/DynamoDBLocal.java
@@ -10,7 +10,10 @@ import static helltech.dynamodb.DatabaseConstants.PK3;
 import static helltech.dynamodb.DatabaseConstants.SK0;
 import static helltech.dynamodb.DatabaseConstants.SK2;
 import static helltech.dynamodb.DatabaseConstants.SK3;
+import static java.util.Objects.isNull;
 import com.amazonaws.services.dynamodbv2.local.embedded.DynamoDBEmbedded;
+import java.util.ArrayList;
+import java.util.Optional;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 import software.amazon.awssdk.services.dynamodb.model.AttributeDefinition;
 import software.amazon.awssdk.services.dynamodb.model.CreateTableRequest;
@@ -22,58 +25,76 @@ import software.amazon.awssdk.services.dynamodb.model.ProjectionType;
 import software.amazon.awssdk.services.dynamodb.model.ProvisionedThroughput;
 import software.amazon.awssdk.services.dynamodb.model.ScalarAttributeType;
 
+/**
+ * A local DynamoDB compatible database.
+ */
 public class DynamoDBLocal {
 
-    public static DynamoDbClient dynamoDBLocal() {
-        var client = DynamoDBEmbedded.create().dynamoDbClient();
-        client.createTable(CreateTableRequest.builder()
-                               .tableName("table")
-                               .provisionedThroughput(getProvisionedThroughput())
-                               .attributeDefinitions(attribute(PK0), attribute(SK0),
-                                                     attribute(PK1), attribute(PK2), attribute(SK2),
-                                                     attribute(PK3), attribute(SK3))
-                               .globalSecondaryIndexes(globalSecondaryIndex(GSI1, PK1),
-                                                       globalSecondaryIndex(GSI2, PK2, SK2),
-                                                       globalSecondaryIndex(GSI3, PK3, SK3))
-                               .keySchema(schemaElement(KeyType.HASH, PK0), schemaElement(KeyType.RANGE, SK0))
-                               .build());
+    /**
+     * A convenience method to create an instance of DynamoDBEmbedded with
+     * a pre-configured table with some defaults for testing purposes and returns a client.
+     *
+     * @param tableName the name of the table to create.
+     * @return A DynamoDBClient instance.
+     */
+    public static DynamoDbClient dynamoDBLocal(String tableName) {
+        var database = DynamoDBEmbedded.create();
+        var client = database.dynamoDbClient();
+        createTable(tableName, client);
         return client;
     }
 
-    private static GlobalSecondaryIndex globalSecondaryIndex(String name, String partitionKey) {
+    private static void createTable(String tableName, DynamoDbClient client) {
+        client.createTable(CreateTableRequest.builder()
+                               .tableName(tableName)
+                               .provisionedThroughput(createProvisionedThroughput())
+                               .attributeDefinitions(createAttribute(PK0), createAttribute(SK0),
+                                                     createAttribute(PK1), createAttribute(PK2), createAttribute(SK2),
+                                                     createAttribute(PK3), createAttribute(SK3))
+                               .globalSecondaryIndexes(createGlobalSecondaryIndex(GSI1, PK1, null),
+                                                       createGlobalSecondaryIndex(GSI2, PK2, SK2),
+                                                       createGlobalSecondaryIndex(GSI3, PK3, SK3))
+                               .keySchema(createKeySchemaElements(PK0, SK0))
+                               .build());
+    }
+
+    private static GlobalSecondaryIndex createGlobalSecondaryIndex(String name, String partitionKey, String sortKey) {
         return GlobalSecondaryIndex.builder()
                    .indexName(name)
-                   .provisionedThroughput(getProvisionedThroughput())
+                   .provisionedThroughput(createProvisionedThroughput())
                    .projection(Projection.builder().projectionType(ProjectionType.ALL).build())
-                   .keySchema(schemaElement(KeyType.HASH, partitionKey))
+                   .keySchema(createKeySchemaElements(partitionKey, sortKey))
                    .build();
     }
 
-    private static GlobalSecondaryIndex globalSecondaryIndex(String name, String partitionKey, String sortKey) {
-        return GlobalSecondaryIndex.builder()
-                   .indexName(name)
-                   .provisionedThroughput(getProvisionedThroughput())
-                   .projection(Projection.builder().projectionType(ProjectionType.ALL).build())
-                   .keySchema(schemaElement(KeyType.HASH, partitionKey),
-                              schemaElement(KeyType.RANGE, sortKey))
-                   .build();
+    private static ArrayList<KeySchemaElement> createKeySchemaElements(String partitionKey, String sortKey) {
+        var keySchemas = new ArrayList<KeySchemaElement>();
+        createSchemaElement(KeyType.HASH, partitionKey).ifPresentOrElse(keySchemas::add, IllegalArgumentException::new);
+        createSchemaElement(KeyType.RANGE, sortKey).ifPresent(keySchemas::add);
+        return keySchemas;
     }
 
-    private static ProvisionedThroughput getProvisionedThroughput() {
+    private static ProvisionedThroughput createProvisionedThroughput() {
         return ProvisionedThroughput.builder()
                    .readCapacityUnits(1000L)
                    .writeCapacityUnits(1000L)
                    .build();
     }
 
-    private static KeySchemaElement schemaElement(KeyType keyType, String attributeName) {
-        return KeySchemaElement.builder()
+    private static Optional<KeySchemaElement> createSchemaElement(KeyType keyType, String attributeName) {
+        return isNullSortKey(keyType, attributeName)
+            ? Optional.empty()
+            : Optional.of(KeySchemaElement.builder()
                    .keyType(keyType)
                    .attributeName(attributeName)
-                   .build();
+                   .build());
     }
 
-    private static AttributeDefinition attribute(String attributeName) {
+    private static boolean isNullSortKey(KeyType keyType, String attributeName) {
+        return KeyType.RANGE == keyType && isNull(attributeName);
+    }
+
+    private static AttributeDefinition createAttribute(String attributeName) {
         return AttributeDefinition.builder()
                    .attributeName(attributeName)
                    .attributeType(ScalarAttributeType.S)
